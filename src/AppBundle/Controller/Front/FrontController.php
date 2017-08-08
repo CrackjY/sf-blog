992d2f3211bf1988d4b7e91a198dfb9618bebd44
yass <yassine.latti@gmail.com>
2017-08-08 13:53:03 +0200

    exo-algo-famille

--- src/AppBundle/Controller/Front/FrontController.php
+++ src/AppBundle/Controller/Front/FrontController.php
@@ -17,8 +17,77 @@ class FrontController extends Controller
             'etienne'
         );
 
+        $peoples =  array(
+                        array(
+                            'firstname' => 'tom',
+                            'lastname' => 'tod',
+                            'children' => array(
+                                array(
+                                    'firstname' => 'uiui',
+                                    'lastname' => 'uouo',
+                                    'age' => 8
+                                ),
+                                array(
+                                    'firstname' => 'qsqqs',
+                                    'lastname' => 'asas',
+                                    'age' => 6
+                                ),
+                                array(
+                                    'firstname' => 'vvbb',
+                                    'lastname' => 'vcvc',
+                                    'age' => 4
+                                )
+                            )
+                        ),
+                        array(
+                            'firstname' => 'dqdq',
+                            'lastname' => 'dqdq',
+                            'children' => array(
+                                array(
+                                    'firstname' => 'uiui',
+                                    'lastname' => 'uouo',
+                                    'age' => 8
+                                    ),
+                                array(
+                                    'firstname' => 'qsqqs',
+                                    'lastname' => 'asas',
+                                    'age' => 6
+                                ),
+                                array(
+                                    'firstname' => 'vvbb',
+                                    'lastname' => 'vcvc',
+                                    'age' => 4
+                                )
+                            )
+                        ),
+                        array(
+                            'firstname' => 'dede',
+                            'lastname' => 'dada',
+                            'children' => array(
+                                array(
+                                    'firstname' => 'uiui',
+                                    'lastname' => 'uouo',
+                                    'age' => 8
+                                ),
+                                array(
+                                    'firstname' => 'qsqqs',
+                                    'lastname' => 'asas',
+                                    'age' => 6
+                                ),
+                                array(
+                                    'firstname' => 'vvbb',
+                                    'lastname' => 'vcvc',
+                                    'age' => 4
+                                )
+                            )
+                        )
+                    );
+
+        var_dump($peoples);
+
         return $this->render(':front:index.html.twig', array(
-            'firstnames' => $firstnames
+            'firstnames' => $firstnames,
+            'peoples' => $peoples
         ));
     }
 
