1da5ab0951701a8d86642a3a858533c18c7f13cf
Yassine <yassine.latti@gmail.com>
2017-09-14 11:19:58 +0200

    Repository-first-method

--- src/AppBundle/Entity/Article/Repository/CommentRepository.php
+++ src/AppBundle/Entity/Article/Repository/CommentRepository.php
@@ -4,7 +4,13 @@ namespace AppBundle\Entity\Article\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class CommentRepository
+ * @package AppBundle\Entity\Article\Repository
+ */
 class CommentRepository extends EntityRepository
 {
+        public function findByTerm($string) {
 
+        }
 }
