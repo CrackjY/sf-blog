7ded23383feb3a3f34b1e29342c36fd129ce7218
Yassine <yassine.latti@gmail.com>
2017-09-28 11:16:47 +0200

    command

--- src/AppBundle/Command/Training/DateCommand.php
+++ src/AppBundle/Command/Training/DateCommand.php
@@ -34,8 +34,8 @@ class DateCommand extends ContainerAwareCommand
 
         $startTime = time();
 
-        $date = \DateTime::createFromFormat('d/m/Y', '27/09/2017');
-        $date->modify('+1 month -1 day');
+        $date = \DateTime::createFromFormat('d/m/Y', '');
+        $date->modify('+1 month');
         $dateFormat = $date->format('d/m/Y');
 
         $input->getArgument('date');
