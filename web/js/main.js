a445ce0ddfd82fc2104640f60ce75bd1ea8277c9
Yassine <yassine.latti@gmail.com>
2017-10-02 10:15:21 +0200

    data-prototype-et-form-imbriqué

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,7 @@
+var tagCount = $('#article_tags___tag___name').length;
+var tagContainer = $('#article_tags');
+var tagForm = tagContainer.attr('data-prototype');
+
 $(function () {
     $('body').on('click', '#show-input-comment-send', function(e) {
         e.preventDefault();
@@ -6,13 +10,8 @@ $(function () {
         $('#show-input-comment-send').remove();
     })
 
-    var tagCount = $('#article_tags___tag___name').length;
-
     $('body').on('click', '#add-tag', function(e) {
         e.preventDefault();
-
-        var tagContainer = $('#article_tags');
-        var tagForm = tagContainer.attr('data-prototype');
 
         tagForm = tagForm.replace(/__tag__/g, tagCount);
 
@@ -20,8 +19,11 @@ $(function () {
 
         tagContainer.append(tagForm);
     })
+})
 
+$(function () {
     $('body').on('click', '#delete-tag', function (e) {
         e.preventDefault();
+
     })
-});
+})
